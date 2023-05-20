e93d6ec7106d6ea5a63c73c4c8f87837a2fbb646
KK-Romanov <122021943+KK-Romanov@users.noreply.github.com>
2023-05-20 05:37:15 +0000

    調整

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
+  devise_for :admin,controllers: {
+  sessions: "admin/sessions"
+}
+#skip: [:registrations, :passwords]  使用検討中
+
+#---------------------------------------------------------------------------
+ 
+  devise_for :users,controllers: {
+  registrations: "public/registrations",
+  sessions: 'public/sessions'
+}
+#skip: [:passwords],   使用検討中
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
